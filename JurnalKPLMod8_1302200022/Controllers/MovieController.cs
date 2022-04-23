d68cbadfdd0c995f8d00bc76ec97c2a95d7ce498
naufalchairurrijal <naufalalifaza@student.telkomuniversity.ac.id>
2022-04-23 11:09:38 +0700

    menambahkan weatherforecasdt

---
+++ JurnalKPLMod8_1302200022/Controllers/MovieController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+
+
+namespace JurnalKPLMod8_1302200022.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    
+    public class MovieController : ControllerBase
+    {
+        private string[] SemuaMovie = new[]
+        {
+            "The Shawshank Redemtion", "The Godfather", "The Dark Knight"
+        };
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return SemuaMovie;
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return SemuaMovie[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        [HttpGet("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpGet("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+}
---
+++ JurnalKPLMod8_1302200022/Movie.cs
@@ -0,0 +1,15 @@
+﻿namespace JurnalKPLMod8_1302200022
+{
+    public class Movie
+    {
+        public DateTime Date { get; set; }
+
+        public string Title { get; set; }
+
+        public string Director { get; set; }
+
+        public string Description { get; set; }
+
+        public string? Summary { get; set; }
+    }
+}
